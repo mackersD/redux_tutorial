533202631cfb68a4c07ad11fc601ee652134979a
Jonathan MacDonald <jonathan@Jonathans-MacBook-Pro.local>
2017-05-15 15:11:23 -0500

    add SET_VISIBILITY_FILTER to reducer

--- reducers.js
+++ reducers.js
@@ -6,5 +6,12 @@ const initialState = {
 };
 
 function todoApp(state = initialState, action) {
-  return state;
+  switch(action.type) {
+    case SET_VISIBILITY_FILTER:
+      return Object.assign({}, state, {
+        visibilityFilter: action.filter
+      });
+    default:
+      return state;
+  }
 };
