0d056d4ff0762b6529168e697f39028784e23a12
Jonathan MacDonald <jonathan@Jonathans-MacBook-Pro.local>
2017-05-15 15:13:24 -0500

    add ADD_TODO to reducer

--- reducers.js
+++ reducers.js
@@ -11,6 +11,16 @@ function todoApp(state = initialState, action) {
       return Object.assign({}, state, {
         visibilityFilter: action.filter
       });
+    case ADD_TODO:
+      return Object.assign({}, state, {
+        todos: [
+          ...state.todos,
+          {
+            text: action.text,
+            completed: false
+          }
+        ]
+      });
     default:
       return state;
   }
